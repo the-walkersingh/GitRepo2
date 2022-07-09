f0453c81c42e40565edc200315a60d6efff92f14
Diwakar Singh <the.walkersingh@hotmail.com>
2022-07-09 23:05:13 +0530

    Version 1.5 Major release

--- GitRepo2/Program.cs
+++ GitRepo2/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello Git Hub");
 Console.WriteLine("This is version 1.3");
+Console.WriteLine("This is version 1.5 and this is a major relese");
